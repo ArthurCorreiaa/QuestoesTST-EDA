6e2221ed60f203912a59bf59ed1086e7500df2aa
Arthur Correia <arthurcorreialeite@gmail.com>
2025-02-26 21:47:31 -0300

    add: MaxPilhaIndex

---
+++ src/MaxPilhaIndex.java
@@ -0,0 +1,62 @@
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class MaxPilhaIndex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] valores = sc.nextLine().split(" ");
+        String indice = sc.nextLine();
+        sc.close();
+
+        Pilha pilha = new Pilha(valores.length);
+        for (int i = 0; i < valores.length; i++)
+            pilha.push(Integer.parseInt(valores[i]));
+
+        System.out.println(pilha.maxPilhaIndex(Integer.parseInt(indice)));
+    }
+
+    public static class Pilha {
+        private int topo;
+        private int[] pilha;
+
+        public Pilha() {
+            this(5);
+        }
+
+        public Pilha(int tamanho) {
+            this.topo = -1;
+            this.pilha = new int[tamanho];
+        }
+
+        public void push(int valor) {
+            if (this.topo == this.pilha.length-1) throw new IndexOutOfBoundsException();
+            this.pilha[++this.topo] = valor;
+        }
+
+        public int pop() {
+            if (this.topo == -1) throw new NoSuchElementException();
+            return this.pilha[this.topo--];
+        }
+
+        public int maxPilhaIndex(int index) {
+            int primeiro = pop();
+            int maior = primeiro;
+            push(primeiro);
+
+            Pilha auxPilha = new Pilha(index+1);
+            for (int i = 0; i <= index; i++) {
+                int valor = pop();
+
+                if (maior < valor)
+                    maior = valor;
+                
+                auxPilha.push(valor);
+            }
+
+            for (int i = 0; i <= index; i++) 
+                push(auxPilha.pop());
+            
+            return maior;
+        }
+    }
+}
