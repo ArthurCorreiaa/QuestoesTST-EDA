a6b827109340b5c4265301aee43a118765930ca0
Arthur Correia <arthurcorreialeite@gmail.com>
2025-03-24 18:59:16 -0300

    fix: Problema de conflito entre as classes

--- src/ContaNosInternosBST.java
+++ src/ContaNosInternosBST.java
@@ -5,6 +5,7 @@ public class ContaNosInternosBST {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] valores = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        sc.close();
     
         BST bst = new BST();
         for (int valor : valores) 
@@ -13,76 +14,76 @@ public class ContaNosInternosBST {
         
         System.out.println(bst.contaNosInternos());
     }
-}
-
-class BST {
-    private Node root;
-    private int size;
-
-    public BST() {
-        root = null;
-        size = 0;
-    }
-
-    public boolean isEmpty() {
-        return root == null;
-    }
-
-    public void add(int value) {
-        size++;
-        if (isEmpty()) root = new Node(value);
-        else {
-            Node currNode = root;
-            while (currNode != null) {
-                if (value < currNode.value) {
-                    if (currNode.left == null) {
-                        Node node = new Node(value);
-                        currNode.left = node;
-                        node.parent = currNode;
-                        return;
+    public static class BST {
+        private Node root;
+        // private int size;
+    
+        public BST() {
+            root = null;
+            // size = 0;
+        }
+    
+        public boolean isEmpty() {
+            return root == null;
+        }
+    
+        public void add(int value) {
+            if (isEmpty()) root = new Node(value);
+            else {
+                Node currNode = root;
+                while (currNode != null) {
+                    if (value < currNode.value) {
+                        if (currNode.left == null) {
+                            Node node = new Node(value);
+                            currNode.left = node;
+                            node.parent = currNode;
+                            return;
+                        }
+                        currNode = currNode.left; 
+                    } else {
+                        if (currNode.right == null) {
+                            Node node = new Node(value);
+                            currNode.right = node;
+                            node.parent = currNode;
+                            return;
+                        }
+                        currNode = currNode.right;
                     }
-                    currNode = currNode.left; 
-                } else {
-                    if (currNode.right == null) {
-                        Node node = new Node(value);
-                        currNode.right = node;
-                        node.parent = currNode;
-                        return;
-                    }
-                    currNode = currNode.right;
                 }
             }
+            // size++;
         }
-    }
-
-    public int contaNosInternos() {
-        Node node = root;
-        return contaNosInternos(node);
-    }
-
-    private int contaNosInternos(Node node) {
-        if (node == null || isFolha(node)) return 0;
- 
-        return 1 + contaNosInternos(node.left) + contaNosInternos(node.right);
-    }
-
-    private boolean isFolha(Node node) {
-        return node.left == null && node.right == null;
-    } 
-
-}
-
-class Node {
-    int value;
-    Node left;
-    Node right;
-    Node parent;
-
-    public Node(int value) {
-        this.value = value;
-        left = null;
-        right = null;
-        parent = null;
+    
+        public int contaNosInternos() {
+            Node node = root;
+            return contaNosInternos(node);
+        }
+    
+        private int contaNosInternos(Node node) {
+            if (node == null || isFolha(node)) return 0;
+     
+            return 1 + contaNosInternos(node.left) + contaNosInternos(node.right);
+        }
+    
+        private boolean isFolha(Node node) {
+            return node.left == null && node.right == null;
+        } 
+    
     }
     
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+        Node parent;
+    
+        public Node(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+            parent = null;
+        }
+        
+    }
 }
+
--- src/PredecessorBst.java
+++ src/PredecessorBst.java
@@ -16,95 +16,94 @@ public class PredecessorBst {
         
         System.out.println(bst.predecessor(num));
     }
-}
-
-class BST {
-    private Node root;
-    private int size;
-
-    public BST() {
-        root = null;
-        size = 0;
-    }
-
-    public boolean isEmpty() {
-        return root == null;
-    }
-
-    public void add(int value) {
-        if (isEmpty()) this.root = new Node(value);
-        else {
-            Node currNode = root;
-            while (currNode != null) {
-                if (value < currNode.value) {
-                    if (currNode.left == null) {
-                        Node node = new Node(value);
-                        currNode.left = node;
-                        node.parent = currNode;
-                        return;
+    public static class BST {
+        private Node root;
+        // private int size;
+    
+        public BST() {
+            root = null;
+            // size = 0;
+        }
+    
+        public boolean isEmpty() {
+            return root == null;
+        }
+    
+        public void add(int value) {
+            if (isEmpty()) this.root = new Node(value);
+            else {
+                Node currNode = root;
+                while (currNode != null) {
+                    if (value < currNode.value) {
+                        if (currNode.left == null) {
+                            Node node = new Node(value);
+                            currNode.left = node;
+                            node.parent = currNode;
+                            return;
+                        }
+                        currNode = currNode.left;
+                    } else {
+                        if (currNode.right == null) {
+                            Node node = new Node(value);
+                            currNode.right = node;
+                            node.parent = currNode;
+                            return;
+                        }
+                        currNode = currNode.right;
                     }
-                    currNode = currNode.left;
-                } else {
-                    if (currNode.right == null) {
-                        Node node = new Node(value);
-                        currNode.right = node;
-                        node.parent = currNode;
-                        return;
-                    }
+                }
+            }
+            // size++;
+        }
+    
+        public ArrayList<Integer> predecessor(int value) {
+            ArrayList<Integer> caminho = new ArrayList<>();
+    
+            Node currNode = encontraNode(value);
+            caminho.add(currNode.value);
+            if (currNode.left == null) {
+                while (currNode.parent != null) {
+                    caminho.add(currNode.parent.value);
+                    if (currNode.value > currNode.parent.value) break;
+    
+                    currNode = currNode.parent;
+                }
+            } else {
+                currNode = currNode.left;
+                while (currNode != null) {
+                    caminho.add(currNode.value);
                     currNode = currNode.right;
                 }
             }
+            return caminho;
         }
-        size++;
-    }
-
-    public ArrayList<Integer> predecessor(int value) {
-        ArrayList<Integer> caminho = new ArrayList<>();
-
-        Node currNode = encontraNode(value);
-        caminho.add(currNode.value);
-        if (currNode.left == null) {
-            while (currNode.parent != null) {
-                caminho.add(currNode.parent.value);
-                if (currNode.value > currNode.parent.value) break;
-
-                currNode = currNode.parent;
-            }
-        } else {
-            currNode = currNode.left;
+    
+        private Node encontraNode(int value) {
+            Node currNode = root;
             while (currNode != null) {
-                caminho.add(currNode.value);
-                currNode = currNode.right;
+                if (currNode.value == value) break;
+    
+                if (value < currNode.value)
+                    currNode = currNode.left;
+                else currNode = currNode.right;
             }
+            return currNode;
         }
-        return caminho;
+    
+    
     }
-
-    private Node encontraNode(int value) {
-        Node currNode = root;
-        while (currNode != null) {
-            if (currNode.value == value) break;
-
-            if (value < currNode.value)
-                currNode = currNode.left;
-            else currNode = currNode.right;
+    
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+        Node parent;
+    
+        public Node(int value) {
+            this.value = value;
+            left = null;
+            right = null;
+            parent = null;
         }
-        return currNode;
-    }
-
-
-}
-
-class Node {
-    int value;
-    Node left;
-    Node right;
-    Node parent;
-
-    public Node(int value) {
-        this.value = value;
-        left = null;
-        right = null;
-        parent = null;
     }
 }
